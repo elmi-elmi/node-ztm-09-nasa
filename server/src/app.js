11bfc3e31207936fa63d4a296b34d265198acfbb
elmi-elmi <shahrokhelmi@gmail.com>
2023-01-05 14:17:44 +0330

    build client

--- server/src/app.js
+++ server/src/app.js
@@ -10,11 +10,11 @@ app.use(cors({
 }))
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, '..', 'front')))
 
 app.use(planetsRouter)
 app.get('/',(req, res)=>{
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+    res.sendFile(path.join(__dirname, '..', 'front', 'index.html'))
 })
 
 module.exports = app;
